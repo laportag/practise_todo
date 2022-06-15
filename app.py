c6a1c5d5e11014cd69523d51be066702f823a788
laportag <greglaporta@hotmail.com>
2022-06-15 11:07:03 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
